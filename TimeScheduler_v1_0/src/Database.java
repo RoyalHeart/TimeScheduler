1e7d3453816aa1ca0262187992da937df8cd6260
Thái Hoàng Tâm <75922889+RoyalHeart@users.noreply.github.com>
2021-12-30 15:27:03 +0100

    Update: add getUser & only connect to database once
    createConnection will be called at the beginning of the program

--- TimeScheduler_v1_0/src/Database.java
+++ TimeScheduler_v1_0/src/Database.java
@@ -8,34 +8,54 @@ import java.util.Scanner;
 public class Database {
     final static String addUser = "INSERT INTO TISCH_USER VALUES (?, ?, ?, ?, ?, ?)";
     final static String databaseUsername = "S1_student2_46";
-    static String databasePassword = "Password";
+    static String databasePassword = getDatabasePassword();
+    static Connection con;
 
-    public static boolean addTischUser(String username, String password, String name, String phone,
-            String email) {
-
-        // load the password from file
+    // create database connection to the Oracle database
+    static Connection createConnection() {
         try {
-            File myObj = new File("ProgramJava/Project/TimeScheduler_v1_0/lib/DatabaseLoginInfo.txt");
+            // step1 load the driver class
+            // Class<?> driverClass = Class.forName("oracle.jdbc.driver.OracleDriver");
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+
+            // step2 create the connection object
+            con = DriverManager.getConnection(
+                    "jdbc:oracle:thin:@db1.fb2.frankfurt-university.de:1521:info01",
+                    databaseUsername,
+                    databasePassword);
+            return con;
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Error");
+            return null;
+        }
+    };
+
+    // read password from file
+    private static String getDatabasePassword() {
+        File myObj = new File("ProgramJava/Project/TimeScheduler_v1_0/lib/DatabaseLoginInfo.txt");
+        try {
             Scanner myReader = new Scanner(myObj);
             while (myReader.hasNextLine()) {
                 databasePassword = myReader.nextLine();
             }
             myReader.close();
+            return databasePassword;
         } catch (FileNotFoundException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
+            return null;
         }
+    }
 
+    // add user to database
+    public static boolean addTischUser(String username, String password, String name, String phone,
+            String email) {
         try {
-            // step1 load the driver class
-            Class.forName("oracle.jdbc.driver.OracleDriver");
+            // create the connection object
+            // Connection con = createConnection();
 
-            // step2 create the connection object
-            Connection con = DriverManager.getConnection(
-                    "jdbc:oracle:thin:@db1.fb2.frankfurt-university.de:1521:info01", databaseUsername,
-                    databasePassword);
-
-            // step3 create the prepared statement object
+            // create the prepared statement object
             PreparedStatement ps = con.prepareStatement(addUser);
             ps.setString(1, "");
             ps.setString(2, username);
@@ -44,12 +64,12 @@ public class Database {
             ps.setString(5, phone);
             ps.setString(6, email);
 
-            // step4 execute query
+            // execute query
             ps.execute();
             ps.close();
 
-            // step5 close the connection object
-            con.close();
+            // close the connection object
+            // con.close();
             return true;
         } catch (Exception e) {
             System.out.println(e);
@@ -57,38 +77,33 @@ public class Database {
         }
     }
 
+    // check if user exists in database
+    // check for login
     static boolean existUser(String username, String hashPassword) {
         System.out.println(username + ' ' + hashPassword);
         try {
             System.out.println(username + ' ' + hashPassword);
-            // step1 load the driver class
-            Class.forName("oracle.jdbc.driver.OracleDriver");
-
-            // step2 create the connection object
-            Connection con = DriverManager.getConnection(
-                    "jdbc:oracle:thin:@db1.fb2.frankfurt-university.de:1521:info01", databaseUsername, "T@m3062002");
-
-            // step3 create the statement object
+            // create statement object
+            // con = createConnection();
             Statement stmt = con.createStatement();
 
-            // step4 execute query
+            // create a query
             ResultSet rs = stmt.executeQuery("SELECT * FROM TISCH_USER WHERE USERNAME = '" + username + "'");
 
+            // execute query
             rs.next();
-            System.out.println(rs.getString(2) + " " + rs.getString(3));
+            // System.out.println(rs.getString(2) + " " + rs.getString(3));
             // step 5 process the result set
+            // if user exists
             if (rs.getString(3).equals(hashPassword.toUpperCase())) {
-                System.out.println(rs.getString(2) + " " + rs.getString(3));
-                con.close();
+                // System.out.println(rs.getString(2) + " " + rs.getString(3));
+                // con.close();
                 return true;
-            } else {
-                System.out.println(rs.getString(2) + " " + rs.getString(3));
-                con.close();
+            } else { // if user doesn't exist
+                // System.out.println(rs.getString(2) + " " + rs.getString(3));
+                // con.close();
                 return false;
             }
-
-            // step 6 close the connection object
-
         } catch (Exception e) {
             System.out.println(e);
             return false;
@@ -97,34 +112,82 @@ public class Database {
 
     static boolean existAdmin(String username, String hashPassword) {
         try {
-            // step1 load the driver class
-            Class.forName("oracle.jdbc.driver.OracleDriver");
+            // create the connection object
+            // Connection con = createConnection();
 
-            // step2 create the connection object
-            Connection con = DriverManager.getConnection(
-                    "jdbc:oracle:thin:@db1.fb2.frankfurt-university.de:1521:info01", databaseUsername, "T@m3062002");
-
-            // step3 create the statement object
+            // create the statement object
             Statement stmt = con.createStatement();
 
-            // step4 execute query
+            // execute query
             ResultSet rs = stmt.executeQuery("SELECT * FROM TISCH_USER WHERE USERNAME = '" + username + "'");
 
-            // step5 process the result set
+            // process the result set
             if (rs.next()) {
-                if (rs.getString(3).equals(hashPassword)) {
+                if (rs.getString(3).equals(hashPassword) && rs.getString(1).equals("0")) {
                     return true;
                 } else {
                     return false;
                 }
             }
 
-            // step6 close the connection object
-            con.close();
+            // close the connection object
+            // con.close();
         } catch (Exception e) {
             System.out.println(e);
         }
-
         return false;
+    }
+
+    static User getUser(String username, String hashPassword) {
+        try {
+            // create the connection object
+            // Connection con = createConnection();
+
+            // create the statement object
+            Statement stmt = con.createStatement();
+
+            // execute query
+            ResultSet rs = stmt.executeQuery("SELECT * FROM TISCH_USER WHERE USERNAME = '" + username + "'");
+
+            // process the result set
+            if (rs.next()) {
+                User user = new User(rs.getString(3), rs.getString(4), rs.getString(5), rs.getString(6));
+                // con.close();
+                return user;
+            }
+
+            // close the connection object
+            // con.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return null;
+    }
+
+    static User getAdmin(String username, String hashPassword) {
+        try {
+
+            // create the connection object
+            // Connection con = createConnection();
+
+            // create the statement object
+            Statement stmt = con.createStatement();
+
+            // execute query
+            ResultSet rs = stmt.executeQuery("SELECT * FROM TISCH_USER WHERE USERNAME = '" + username + "'");
+
+            // process the result set
+            if (rs.next() && rs.getString(1).equals("0")) {
+                User user = new User(rs.getString(3), rs.getString(4), rs.getString(5), rs.getString(6));
+                // con.close();
+                return user;
+            }
+
+            // close the connection object
+            // con.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return null;
     }
 }
