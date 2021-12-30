f6944c215cb95abdf8e318f3a31f27c604599c50
RoyalHeart <hoangtam3062002@gmail.com>
2021-12-30 16:22:58 +0100

    Update: add parameter user Because each user have different view.

--- TimeScheduler_v1_0/src/MainFrame.java
+++ TimeScheduler_v1_0/src/MainFrame.java
@@ -6,39 +6,34 @@ import java.awt.event.ActionListener;
 
 import javax.swing.*;
 
-
 public class MainFrame extends JFrame {
     ImageIcon icon = new ImageIcon("ProgramJava/Project/TimeScheduler_v1_0/libTimeSchedulerIcon.png");
 
-    MainFrame() {
+    MainFrame(User user) {
         this.setIconImage(icon.getImage());
-        JPanel calendar = new SwingCalendar();
         this.setSize(600, 700);
         this.setPreferredSize(new Dimension(600, 700));
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setTitle("Time Scheduler");
         this.setLayout(new BorderLayout());
-        this.add(calendar, BorderLayout.CENTER);
-        this.add(new Navigation(calendar, this), BorderLayout.WEST);
+        this.add(new Navigation(this, user), BorderLayout.WEST);
         JButton addButton = new JButton("+");
-        addButton.addActionListener(new ActionListener()
-        {
+        addButton.addActionListener(new ActionListener() {
 
             @Override
             public void actionPerformed(ActionEvent e) {
                 // TODO Auto-generated method stub
                 new AddEventDialog();
             }
-            
+
         });
         this.add(addButton, BorderLayout.EAST);
         this.setVisible(true);
     }
-    
-   
 
     public static void main(String[] args) {
-        new MainFrame();
+        Database.createConnection();
+        new MainFrame(Database.getUser("abc", Hash.hashPassword("abc").toUpperCase()));
     }
 }
