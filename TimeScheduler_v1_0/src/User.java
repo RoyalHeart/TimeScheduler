29a6fa4015849a141165f9678f8316c2b42b936f
Thái Hoàng Tâm <75922889+RoyalHeart@users.noreply.github.com>
2021-12-30 15:50:03 +0100

    Add: User class

---
+++ TimeScheduler_v1_0/src/User.java
@@ -0,0 +1,56 @@
+package src;
+
+public class User {
+    private String Id;
+    private String username;
+    private String email;
+    private String phone;
+    private String name;
+
+    public User(String username, String name, String email, String phone) {
+        this.username = username;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public String getId() {
+        return Id;
+    }
+
+    public void setId(String id) {
+        this.Id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
