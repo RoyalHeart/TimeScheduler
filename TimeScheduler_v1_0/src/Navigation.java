9aa6ab2f9aebf67bf8ce350028913e31fc8946c8
RoyalHeart <hoangtam3062002@gmail.com>
2021-12-30 19:57:36 +0100

    Add: image to each button

--- TimeScheduler_v1_0/src/Navigation.java
+++ TimeScheduler_v1_0/src/Navigation.java
@@ -6,6 +6,18 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Navigation extends JPanel {
+    private ImageIcon homeImageIcon = new ImageIcon("TimeScheduler_v1_0/src/Images/homeIcon.png");
+    private Image homeImage = homeImageIcon.getImage().getScaledInstance(30, 30, Image.SCALE_AREA_AVERAGING);
+    private Icon homeIcon = new ImageIcon(homeImage);
+
+    private ImageIcon profileImageIcon = new ImageIcon("TimeScheduler_v1_0/src/Images/profileIcon.jpg");
+    private Image profileImage = profileImageIcon.getImage().getScaledInstance(30, 30, Image.SCALE_SMOOTH);
+    private Icon profileIcon = new ImageIcon(profileImage);
+
+    private ImageIcon logoutImageIcon = new ImageIcon("TimeScheduler_v1_0/src/Images/logoutIcon.png");
+    private Image logoutImage = logoutImageIcon.getImage().getScaledInstance(30, 30, Image.SCALE_AREA_AVERAGING);
+    private Icon logoutIcon = new ImageIcon(logoutImage);
+    Dimension size = new Dimension(40, 30);
 
     Navigation(JFrame frame, User user) {
         // panel in navigation
@@ -16,9 +28,9 @@ public class Navigation extends JPanel {
         frame.add(calendar, BorderLayout.CENTER);
         setLayout(new GridLayout(6, 1));
 
-        JButton home = new JButton("Home");
-        home.setPreferredSize(new Dimension(70, 20));
-        home.addActionListener(new ActionListener() {
+        JButton homeButton = new JButton(homeIcon);
+        homeButton.setPreferredSize(size);
+        homeButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent ae) {
                 profilePanel.setVisible(false);
                 frame.remove(profilePanel);
@@ -27,8 +39,8 @@ public class Navigation extends JPanel {
             }
         });
 
-        JButton profileButton = new JButton("Profile");
-        profileButton.setPreferredSize(new Dimension(70, 20));
+        JButton profileButton = new JButton(profileIcon);
+        profileButton.setPreferredSize(size);
 
         profileButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent ae) {
@@ -39,9 +51,9 @@ public class Navigation extends JPanel {
             }
         });
 
-        JButton logout = new JButton("Logout");
-        logout.setPreferredSize(new Dimension(70, 20));
-        logout.addActionListener(new ActionListener() {
+        JButton logoutButton = new JButton(logoutIcon);
+        logoutButton.setPreferredSize(size);
+        logoutButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent ae) {
                 int respone = JOptionPane.showOptionDialog(null, "Are you sure you want to logout?", "Logout",
                         JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, null, null);
@@ -56,10 +68,9 @@ public class Navigation extends JPanel {
             }
         });
 
-        this.add(home);
+        this.add(homeButton);
         this.add(profileButton);
-        this.add(logout);
-        this.setVisible(true);
+        this.add(logoutButton);
         this.setVisible(true);
     }
 }
