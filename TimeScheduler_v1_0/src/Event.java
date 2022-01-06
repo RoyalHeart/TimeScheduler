25f03eb570def7b72ae4cefeb3253b5b94617410
FrostJ143 <70150272+FrostJ143@users.noreply.github.com>
2022-01-06 17:24:56 +0700

    Add event class

---
+++ TimeScheduler_v1_0/src/Event.java
@@ -0,0 +1,55 @@
+package src;
+
+import java.util.Date;
+
+public class Event {
+    private String ID;
+    private String userID;
+    private String title;
+    private String description;
+    private Date date;
+    private String location;
+    private int duration;
+    private int priority;
+    private int remind;
+
+    public Event(String userID, String title, String description, Date date, String location, int duration, int priority, int remind)
+    {
+        this.userID = userID;
+        this.title = title;
+        this.description = description;
+        this.date = date;
+        this.location = location;
+        this.duration = duration;
+        this.priority = priority;
+        this.remind = remind;
+    }
+
+    public String getID() {return ID;}
+    public void setID(String ID) {this.ID = ID;}
+
+    public String getUserID() {return userID;}
+    public void setUserID(String userID) {this.userID = userID;}
+
+    public String getTitle() {return title;}
+    public void setTitle(String title) {this.title = title;}
+
+    public String getDescription() {return description;}
+    public void setDescription(String descripton) {this.description = descripton;}
+
+    public Date getDate() {return date;}
+    public void setDate(Date date) {this.date = date;}
+
+    public String getLocation() {return location;}
+    public void setLocaction(String location) {this.location = location;}
+
+    public int getDuration() {return duration;}
+    public void setDuration(int duration) {this.duration = duration;}
+
+    public int getPriority() {return priority;}
+    public void setPriority(int priority) {this.priority = priority;}
+
+    public int getRemind() {return remind;}
+    public void setRemind(int remind) {this.remind = remind;}
+    
+}
