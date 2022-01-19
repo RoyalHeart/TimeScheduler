7efae277f963c14314e7da0fb8ce0cc738acac09
Thái Hoàng Tâm <75922889+RoyalHeart@users.noreply.github.com>
2022-01-19 20:13:10 +0100

    Update: send remind email

--- TimeScheduler_v1_0/src/Mail.java
+++ TimeScheduler_v1_0/src/Mail.java
@@ -1,20 +1,23 @@
 package src;
 
-import javax.mail.*;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Properties;
 import java.util.Random;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 // Mail class is used to send email to the user
 public class Mail {
     private static String verifyCode = "";
     private static final String username = "notification.tisch@gmail.com";
-    private static final String password = "mwtuvjadrnmomeqb";
-
-    static final String reminderSubject = "Reminder from TISCH";
-    static final String reminderBody = "You have a meeting on";
-    static final String generalBody = "Dear user,\n\n" + reminderBody;
+    private static final String password = "trhwsetebngtbeqx";
 
     // create a properties object to store host information
     private static Properties createProperty() {
@@ -82,6 +85,37 @@ public class Mail {
 
     public static void setVerifyCode(String verifyCode) {
         Mail.verifyCode = verifyCode;
+    }
+
+    public static boolean sendRemindEmail(User user, Event event) {
+        Properties prop = createProperty();
+        Session session = createSession(prop);
+        DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        DateFormat timeFormat = new SimpleDateFormat("HH:mm");
+        try {
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(username));
+            message.setRecipients(
+                    Message.RecipientType.TO,
+                    InternetAddress.parse(user.getEmail()));
+            message.setSubject("Reminder from TISCH");
+            message.setText("Dear user " + user.getName() + "\n\n" +
+                    "You have an event on " + dateFormat.format(event.getDate()) + " at "
+                    + timeFormat.format(event.getDate()) + "\n" +
+                    "Your event information: \n" +
+                    "Title: " + event.getTitle() + "\n" +
+                    "Description: " + event.getDescription() + "\n" +
+                    "Location: " + event.getLocation() + "\n" +
+                    "Priority: " + event.getPriority() + "\n" +
+                    "Duration: " + event.getDuration() + " minutes" + "\n\n" +
+                    "TISCH Team");
+            Transport.send(message);
+            System.out.println("Message sent to " + user.getEmail());
+            return true;
+        } catch (MessagingException e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 
     public static void sendEmail(String to, String subject, String body) {
