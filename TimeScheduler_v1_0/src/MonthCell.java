32ada8dcf77f190bf7f697213877ed648f8312e3
Thái Hoàng Tâm <75922889+RoyalHeart@users.noreply.github.com>
2022-01-19 20:29:06 +0100

    Update: add "More" button, each event is a button
    The "More" button is used to show a dialog that shows all events on a day with more than 3 events.
    Each event is clickable and shows basic information.

--- TimeScheduler_v1_0/src/MonthCell.java
+++ TimeScheduler_v1_0/src/MonthCell.java
@@ -1,20 +1,30 @@
 package src;
 
-import java.util.Date;
-import java.util.List;
-import java.util.ArrayList;
-import javax.swing.table.*;
-import javax.swing.*;
-import java.awt.*;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 // import java.util.Locale;
 import java.text.DateFormat;
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.time.*;
-import java.awt.event.*;
-import java.util.Random;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.AbstractCellEditor;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.SwingConstants;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableCellEditor;
+import javax.swing.table.TableCellRenderer;
 
 class MonthCell {
     public String date;
@@ -73,138 +83,6 @@ class MonthCell {
         return this.events;
     }
 }
-// public static void main(ArrayList<String> args) {
-// // Database.createConnection();
-// // List<MonthCell> list = Database.getEvents(new Date(), new Date());
-// ArrayList<List<MonthCell>> data = new ArrayList<List<MonthCell>>();
-// Calendar cal = new GregorianCalendar();
-// JFrame frame = new JFrame();
-// frame.setSize(800, 600);
-// frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-// frame.setLocationRelativeTo(null);
-// frame.setLayout(new BorderLayout());
-
-// cal.set(Calendar.DAY_OF_MONTH, 1);
-// int startDay = cal.get(Calendar.DAY_OF_WEEK);
-// int numberOfDays = cal.getActualMaximum(Calendar.DAY_OF_MONTH);
-// int weeks = cal.getActualMaximum(Calendar.WEEK_OF_MONTH);
-// int day = 1;
-
-// for (int i = 0; i < weeks; i++) {
-// List<MonthCell> list = new ArrayList<MonthCell>();
-// for (int j = 0; j < 7; j++) {
-// if (i == 0 && j < startDay - 1) {
-// list.add(new MonthCell());
-// } else if (day > numberOfDays) {
-// list.add(new MonthCell());
-// } else {
-// list.add(new MonthCell(String.valueOf(day)));
-// day++;
-// }
-// }
-// data.add(list);
-// }
-// // model.setValueAt(value, rowIndex, columnIndex);
-// MyClassTableModel model = new MyClassTableModel(data);
-// JTable myClassTable = new JTable(model);
-
-// myClassTable.setDefaultRenderer(MonthCell.class, new MyClassCellRenderer());
-// // myClassTable.setDefaultEditor(MonthCell.class, new MyClassCellEditor());
-
-// myClassTable.setRowHeight(100);
-// JScrollPane pane = new JScrollPane(myClassTable);
-// JButton button = new JButton("test");
-// frame.add(pane, BorderLayout.CENTER);
-// frame.add(button, BorderLayout.SOUTH);
-// frame.setVisible(true);
-
-// button.addActionListener(new ActionListener() {
-// @Override
-// public void actionPerformed(ActionEvent e) {
-// frame.remove(pane);
-// frame.remove(button);
-// ArrayList<List<MonthCell>> data = new ArrayList<List<MonthCell>>();
-// // System.out.println(cell.date);
-// cal.add(Calendar.MONTH, +1);
-// cal.set(Calendar.DAY_OF_MONTH, 1);
-// cal.set(Calendar.DAY_OF_MONTH, 1);
-// int startDay = cal.get(Calendar.DAY_OF_WEEK);
-// int numberOfDays = cal.getActualMaximum(Calendar.DAY_OF_MONTH);
-// int weeks = cal.getActualMaximum(Calendar.WEEK_OF_MONTH);
-// int day = 1;
-// int month = cal.get(Calendar.MONTH);
-// // User user = Database.getUser("abc", Hash.hashPassword("aabc"));
-// ArrayList<Event> events = new ArrayList<Event>();
-// // create 10 random events
-// for (int i = 0; i < 10; i++) {
-// try {
-// Random rand = new Random();
-// // rand.nextInt() % 12 + 1
-// String dateString = rand.nextInt() % 28 + 1 + "-" + rand.nextInt() % 12 + 1 +
-// "-"
-// + 2022;
-// DateFormat df = new SimpleDateFormat("dd-MM-yyyy");
-// String title = "title" + i;
-// // String time = "time" + i;
-// Date date = df.parse(dateString);
-// Date startTime = new Date(date.getTime() + rand.nextInt() % 100 * 60 * 60 *
-// 1000);
-// int duration = rand.nextInt() % 10 + 1;
-// // int priority = rand.nextInt() % 3;
-// Event event = new Event("0", title, date, startTime, duration);
-// events.add(event);
-// } catch (ParseException e1) {
-// System.out.println("Error");
-// }
-// }
-// int k = 0;
-// ArrayList<String> titles = new String[times.length];
-// for (Event event : events) {
-// System.out.println("Date: " + event.getDate() + " Titles: " +
-// event.getTitle());
-// LocalDate localDate =
-// event.getDate().toInstant().atZone(ZoneId.systemDefault())
-// .toLocalDate();
-
-// if (localDate.getMonthValue() == month && localDate.getDayOfMonth() == day) {
-// titles[k] = event.getTitle();
-// k++;
-// }
-// }
-
-// for (int i = 0; i < weeks; i++) {
-// List<MonthCell> list = new ArrayList<MonthCell>();
-// for (int j = 0; j < 7; j++) {
-// if (i == 0 && j < startDay - 1) {
-// list.add(new MonthCell("", timesEmpty, titlesEmpty));
-// } else if (day > numberOfDays) {
-// list.add(new MonthCell("", timesEmpty, titlesEmpty));
-// } else {
-// list.add(new MonthCell(String.valueOf(day), times, titles));
-// day++;
-// }
-// }
-// data.add(list);
-// }
-// // model.setValueAt(value, rowIndex, columnIndex);
-// MyClassTableModel model = new MyClassTableModel(data);
-// JTable myClassTable = new JTable(model);
-// myClassTable.setDefaultRenderer(MonthCell.class, new MyClassCellRenderer());
-// myClassTable.setRowHeight(100);
-// JScrollPane pane = new JScrollPane(myClassTable);
-// frame.add(pane, BorderLayout.CENTER);
-// frame.add(button, BorderLayout.SOUTH);
-// frame.setVisible(true);
-// // if (y < 6) {
-// // y++;
-// // } else if (x < model.getRowCount() - 1) {
-// // y = 0;
-// // x++;
-// // }
-// }
-// });
-// }
-// }
 
 class MyClassTableModel extends DefaultTableModel {
     private ArrayList<List<MonthCell>> data;
@@ -256,18 +134,15 @@ class MyClassCellComponent extends JPanel {
     JLabel dateLabel = new JLabel();
     JPanel datePanel = new JPanel();
     JScrollPane scrollPane = new JScrollPane();
-    // JLabel timeLabel = new JLabel();
     ArrayList<JLabel> timeLabels = new java.util.ArrayList<JLabel>();
-    // JLabel titleLabel = new JLabel();
     ArrayList<JLabel> titleLabels = new java.util.ArrayList<JLabel>();
-    JPanel eventPanel = new JPanel(new FlowLayout(FlowLayout.LEFT)); // event has time and title
     JPanel eventsPanel = new JPanel(); // eventPanel has events
+    JButton button = new JButton("More"); // button to show all events if there are more than 3
 
     public MyClassCellComponent(MonthCell myClass) {
         // initialize components (labels, buttons, etc.)
         this.setLayout(new BorderLayout());
         dateLabel = new JLabel(myClass.date);
-
         datePanel.setLayout(new FlowLayout(FlowLayout.CENTER));
         datePanel.add(dateLabel);
 
@@ -283,59 +158,66 @@ class MyClassCellComponent extends JPanel {
             titleLabels.add(titleLabel);
         }
 
-        eventsPanel.setLayout(new BoxLayout(eventsPanel, BoxLayout.Y_AXIS));
         for (int i = 0; i < myClass.events.size(); i++) {
-            eventPanel = new JPanel(new FlowLayout(FlowLayout.LEFT)); // eventPanel has time and title
-            eventPanel.setAlignmentX(Component.LEFT_ALIGNMENT);
-            eventPanel.add(timeLabels.get(i));
-            eventPanel.add(titleLabels.get(i));
-            eventsPanel.add(eventPanel); // add each event Panel to eventsPanel
+            String time = timeLabels.get(i).getText();
+            String title = titleLabels.get(i).getText();
+            JButton eventButton = new JButton(time + " " + title);
+            eventButton.setBorderPainted(false);
+            eventButton.setContentAreaFilled(false);
+            eventButton.setFocusPainted(false);
+            eventButton.setHorizontalAlignment(SwingConstants.LEFT);
+            eventButton.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    JOptionPane.showMessageDialog(null, "Event: " + title + "\nTime: " + time);
+                }
+            });
+            eventsPanel.add(eventButton); // add each event Button to eventsPanel
+
         }
 
         this.add(datePanel, BorderLayout.NORTH);
-        scrollPane = new JScrollPane(eventsPanel);
         this.add(eventsPanel, BorderLayout.CENTER);
         this.setPreferredSize(new Dimension(100, 100));
         this.setSize(100, 100);
+
+        if (myClass.events.size() > 3) {
+            button = new JButton("More");
+            this.add(button, BorderLayout.SOUTH);
+            button.addActionListener(l -> {
+                JDialog dialog = new JDialog();
+                dialog.setSize(100, 40 * myClass.events.size());
+                dialog.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+                dialog.setLocationRelativeTo(button);
+                dialog.setVisible(true);
+                dialog.setLayout(new BorderLayout());
+                dialog.add(datePanel, BorderLayout.NORTH);
+                dialog.add(eventsPanel, BorderLayout.CENTER);
+                dialog.setVisible(true);
+            });
+            // JScrollPane scrollPane = new JScrollPane(eventsPanel);
+            // this.add(scrollPane, BorderLayout.CENTER);
+        }
         // add action listeners
     }
 
-    public void updateData(MonthCell myClass, boolean isSelected, JTable table, int row, int column) {
-        // this.data.get(row).set(column, myClass);
-
-        dateLabel = new JLabel(myClass.date);
-
-        for (String time : myClass.times) {
-            JLabel timeLabel = new JLabel(time);
-            timeLabel.setHorizontalAlignment(SwingConstants.LEFT);
-            timeLabels.add(timeLabel);
-        }
-
-        for (String title : myClass.titles) {
-            JLabel titleLabel = new JLabel(title);
-            titleLabel.setHorizontalAlignment(SwingConstants.LEFT);
-            titleLabels.add(titleLabel);
-        }
-
+    public void updateData(MonthCell myClass, boolean isSelected) {
         // add action listeners
     }
 }
 
 class MyClassCellRenderer implements TableCellRenderer {
     MyClassCellComponent panel;
-    // ArrayList<List<MonthCell>> data;
+    static MonthCell monthCell;
 
     public MyClassCellRenderer() {
-    }
 
-    public MyClassCellRenderer(MonthCell myClass) {
-        panel = new MyClassCellComponent(myClass);
     }
 
     public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus,
             int row, int column) {
         // value = table.get(row).get(column);
         MonthCell myClass = (MonthCell) value;
+        monthCell = myClass;
         panel = new MyClassCellComponent(myClass);
         return panel;
     }
@@ -343,22 +225,26 @@ class MyClassCellRenderer implements TableCellRenderer {
 
 class MyClassCellEditor extends AbstractCellEditor implements TableCellEditor {
     MyClassCellComponent panel;
-    JScrollPane scrollPane;
+    MonthCell monthCell;
 
     public MyClassCellEditor() {
-    }
-
-    public MyClassCellEditor(MonthCell myClass) {
-        panel = new MyClassCellComponent(myClass);
     }
 
     public Component getTableCellEditorComponent(JTable table, Object value,
             boolean isSelected, int row, int column) {
         // scrollPane = new JScrollPane(panel);
+        MonthCell myClass = (MonthCell) value;
+        monthCell = myClass;
+        System.out.println("Date: " + myClass.date);
+        System.out.println("Times: " + myClass.times);
+        System.out.println("Titles: " + myClass.titles + "\n");
+        panel = new MyClassCellComponent(myClass);
+        panel.updateData(myClass, isSelected);
+        System.out.println("Cell" + row + "," + column + " is selected: " + isSelected);
         return panel;
     }
 
     public Object getCellEditorValue() {
-        return null;
+        return monthCell;
     }
 }
