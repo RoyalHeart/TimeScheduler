63828707a95fc44be7e0d5a7b4abbd5901d7693b
Thái Hoàng Tâm <75922889+RoyalHeart@users.noreply.github.com>
2022-01-20 15:31:45 +0100

    Update: create scheduler, set LookAndFeel

--- TimeScheduler_v1_0/src/MainFrame.java
+++ TimeScheduler_v1_0/src/MainFrame.java
@@ -1,18 +1,22 @@
 package src;
 
-import java.awt.*;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.*;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.UIManager;
 
 public class MainFrame extends JFrame {
     ImageIcon icon = new ImageIcon("TimeScheduler_v1_0/lib/TimeSchedulerIcon.png");
 
     MainFrame(User user) {
         this.setIconImage(icon.getImage());
-        this.setSize(600, 700);
-        this.setPreferredSize(new Dimension(600, 700));
+        this.setSize(700, 700);
+        this.setPreferredSize(new Dimension(700, 700));
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setTitle("Time Scheduler");
@@ -32,8 +36,15 @@ public class MainFrame extends JFrame {
         this.setVisible(true);
     }
 
-    public static void main(String[] args) {
-        Database.createConnection();
-        new MainFrame(Database.getUser("admin", Hash.hashPassword("adminadmin").toUpperCase()));
+    public static void main(String[] args) throws Exception {
+        try {
+            SchedulerJava.createScheduler();
+            Database.createConnection();
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            // UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+            new MainFrame(Database.getUser("admin", "D82494F05D6917BA02F7AAA29689CCB444BB73F20380876CB05D1F37537B7892");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
