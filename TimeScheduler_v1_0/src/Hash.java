f31fb955d83566ae9f8df96f354a3424ca6aab09
Thái Hoàng Tâm <75922889+RoyalHeart@users.noreply.github.com>
2022-01-23 21:54:06 +0100

    Update: clean up code

--- TimeScheduler_v1_0/src/Hash.java
+++ TimeScheduler_v1_0/src/Hash.java
@@ -1,28 +1,19 @@
 package src;
 
 import java.security.MessageDigest;
-// import java.util.Base64;
 import org.apache.commons.codec.binary.Hex;
 
-// import javax.xml.bind.DatatypeConverter;
-
 public class Hash {
-
     public static String hashPassword(String password) throws RuntimeException {
         try {
-
             MessageDigest md = MessageDigest.getInstance("SHA-256");
             md.update(password.getBytes());
             byte[] digest = md.digest();
-            // String myHash64 = Base64.getEncoder().encodeToString(digest);
             String myHashHex = Hex.encodeHexString(digest);
-            // System.out.println(myHash64);
-            // System.out.println(myHashHex);
             return myHashHex;
         } catch (Exception e) {
             e.printStackTrace();
             return null;
         }
     }
-
 }
