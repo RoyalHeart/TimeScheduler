d9d85692c510ce01666c41d4e961c98573c324c5
vi3tproa7 <vi2tproa7@gmail.com>
2022-01-26 18:41:13 +0100

    Add AdminInterface

---
+++ TimeScheduler_v1_0/src/AdminInterface.java
@@ -0,0 +1,148 @@
+package src;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.JLabel;
+import javax.swing.JTable;
+import javax.swing.JScrollPane;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.beans.Statement;
+import java.security.PublicKey;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.util.Vector;
+import java.awt.event.ActionEvent;
+
+public class AdminInterface extends JFrame {
+
+	private JPanel contentPane;
+	private JTable table;
+	private JButton btnEdit;
+	private JButton btnDelete;
+	DefaultTableModel tableModel;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					AdminInterface frame = new AdminInterface();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public AdminInterface() {
+		setTitle("Admin Interface");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("User List");
+		lblNewLabel.setBounds(20, 11, 65, 14);
+		contentPane.add(lblNewLabel);
+		
+		// temporary code for testing
+		/* need to delete after integrate */
+		Database.createConnection();
+
+		
+		// Initialize the table
+		Vector<String> columnNames = new Vector<String>();
+		Vector<Vector> data = Database.getUserList();
+		
+		columnNames.addElement("ID");
+		columnNames.addElement("Username");
+		columnNames.addElement("Full Name");
+		columnNames.addElement("Email");
+			
+		DefaultTableModel tableModel  = new DefaultTableModel(data, columnNames);
+		table = new JTable(tableModel);
+		
+		JScrollPane scrollPane = new JScrollPane(table);
+		scrollPane.setBounds(20, 34, 392, 200);
+		contentPane.add(scrollPane);
+		
+		btnEdit = new JButton("Edit");
+		btnEdit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				processEdit();
+			}
+		});
+		btnEdit.setBounds(224, 7, 89, 23);
+		contentPane.add(btnEdit);
+		
+		btnDelete = new JButton("Delete");
+		btnDelete.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				processDelete();
+			}
+		});
+	
+		btnDelete.setBounds(323, 7, 89, 23);
+		contentPane.add(btnDelete);
+		
+	}
+	
+	void processEdit() {
+		
+	}
+	
+	void processDelete() {
+		
+	}
+}
+
+/*
+{
+@Override
+public Class getColumnClass(int column)
+{
+	for(int row = 0; row < getRowCount(); row++)
+	{
+		Object o = getValueAt(row, column);
+				
+		if(o != null)
+		{
+			return o.getClass();
+		}
+	}
+	
+return Object.class;
+}
+};*/
+
+//Get data
+		/*
+		Connection connection = Database.createConnection();
+		String sql = "SELECT ID, USERNAME, USERFULLNAME, USEREMAIL FROM TISCH_USER";
+		java.sql.Statement stm = connection.createStatement();
+		ResultSet rs = stm.executeQuery( sql );
+		
+		while (rs.next()){
+			Vector<Object> row = new Vector<Object>(4);
+			
+			for(int i = 0; i < 4; i++) {
+				row.addElement(rs.getObject(i));
+			}
+			
+			data.addElement(row);
+		}
+		*/
