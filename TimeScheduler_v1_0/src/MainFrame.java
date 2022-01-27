e6d811ae3db20e035923717f8cdb98d16e812938
Thái Hoàng Tâm <75922889+RoyalHeart@users.noreply.github.com>
2022-01-27 23:25:23 +0100

    Add: closing window message, Scheduler running

--- TimeScheduler_v1_0/src/MainFrame.java
+++ TimeScheduler_v1_0/src/MainFrame.java
@@ -4,21 +4,27 @@ import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.UIManager;
 
 public class MainFrame extends JFrame {
     ImageIcon icon = new ImageIcon("TimeScheduler_v1_0/lib/TimeSchedulerIcon.png");
 
     MainFrame(User user) {
+        SchedulerJava.createScheduler();
+        SchedulerJava.setFutureRemind(user);
         this.setIconImage(icon.getImage());
         this.setSize(700, 700);
         this.setPreferredSize(new Dimension(700, 700));
+        this.setMinimumSize(new Dimension(580, 280));
         this.setLocationRelativeTo(null);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
         this.setTitle("Time Scheduler");
         this.setLayout(new BorderLayout());
         this.add(new Navigation(this, user), BorderLayout.WEST);
@@ -33,16 +39,33 @@ public class MainFrame extends JFrame {
 
         });
         this.add(addButton, BorderLayout.EAST);
+        this.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                int respone = JOptionPane.showOptionDialog(null,
+                        "Are you sure you want to exit?\nIf you exit no reminder will be sent", "Exit",
+                        JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, null, null);
+                if (respone == JOptionPane.YES_OPTION) {
+                    Database.closeConnection();
+                    SchedulerJava.closeScheduler();
+                    System.exit(0);
+                } else if (respone == JOptionPane.NO_OPTION) {
+                    // do nothing
+                } else {
+                    // do nothing
+                }
+            }
+        });
         this.setVisible(true);
     }
 
     public static void main(String[] args) throws Exception {
         try {
-            SchedulerJava.createScheduler();
             Database.createConnection();
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-            // UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
-            new MainFrame(Database.getUser("admin", "D82494F05D6917BA02F7AAA29689CCB444BB73F20380876CB05D1F37537B7892"));
+            new MainFrame(
+                    Database.getUser("admin", "admin"));
+            // SchedulerJava.createScheduler();
         } catch (Exception e) {
             e.printStackTrace();
         }
