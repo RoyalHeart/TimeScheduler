f91f5d8eb6432d8f77724568c36134dd52cb999f
Thái Hoàng Tâm <75922889+RoyalHeart@users.noreply.github.com>
2022-02-06 09:35:04 +0100

    Add: javadoc, settings screen

--- TimeScheduler_v1_0/src/Navigation.java
+++ TimeScheduler_v1_0/src/Navigation.java
@@ -5,53 +5,70 @@ import javax.swing.*;
 // import java.util.*;
 import java.awt.*;
 
+/**
+ * {@code Navigation} class extends {@link JPanel} is used to navigate between
+ * different screens
+ * 
+ * @author Tam Thai Hoang
+ */
 public class Navigation extends JPanel {
-    private ImageIcon homeImageIcon = new ImageIcon("TimeScheduler_v1_0/src/Images/homeIcon.png");
-    private Image homeImage = homeImageIcon.getImage().getScaledInstance(30, 30, Image.SCALE_AREA_AVERAGING);
+    private ImageIcon homeImageIcon = new ImageIcon("TimeScheduler_v1_0/src/Images/homeIconTransparent.png");
+    private Image homeImage = homeImageIcon.getImage().getScaledInstance(30, 30, Image.SCALE_SMOOTH);
     private Icon homeIcon = new ImageIcon(homeImage);
 
-    private ImageIcon profileImageIcon = new ImageIcon("TimeScheduler_v1_0/src/Images/profileIcon.jpg");
+    private ImageIcon profileImageIcon = new ImageIcon("TimeScheduler_v1_0/src/Images/profileIconTransparent.png");
     private Image profileImage = profileImageIcon.getImage().getScaledInstance(30, 30, Image.SCALE_SMOOTH);
     private Icon profileIcon = new ImageIcon(profileImage);
 
-    private ImageIcon logoutImageIcon = new ImageIcon("TimeScheduler_v1_0/src/Images/logoutIcon.png");
-    private Image logoutImage = logoutImageIcon.getImage().getScaledInstance(30, 30, Image.SCALE_AREA_AVERAGING);
+    private ImageIcon logoutImageIcon = new ImageIcon("TimeScheduler_v1_0/src/Images/logoutIconTransparent.png");
+    private Image logoutImage = logoutImageIcon.getImage().getScaledInstance(30, 30, Image.SCALE_SMOOTH);
     private Icon logoutIcon = new ImageIcon(logoutImage);
-    Dimension size = new Dimension(40, 30);
+
+    private ImageIcon settingsImageIcon = new ImageIcon("TimeScheduler_v1_0/src/Images/settingsIconTransparent.png");
+    private Image settingsImage = settingsImageIcon.getImage().getScaledInstance(30, 30, Image.SCALE_SMOOTH);
+    private Icon settingsIcon = new ImageIcon(settingsImage);
+    private Dimension size = new Dimension(40, 30);
 
     Navigation(JFrame frame, User user) {
         // panel in navigation
         SwingCalendar calendar = new SwingCalendar(user);
         Profile profilePanel = new Profile(user);
-
+        Settings settingsPanel = new Settings(user);
         // default panel
         frame.add(calendar, BorderLayout.CENTER);
         setLayout(new GridLayout(6, 1));
 
         JButton homeButton = new JButton(homeIcon);
+        homeButton.setOpaque(false);
         homeButton.setPreferredSize(size);
         homeButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent ae) {
                 profilePanel.setVisible(false);
+                settingsPanel.setVisible(false);
                 frame.remove(profilePanel);
+                frame.remove(settingsPanel);
                 frame.add(calendar, BorderLayout.CENTER);
                 calendar.setVisible(true);
             }
         });
 
         JButton profileButton = new JButton(profileIcon);
+        profileButton.setOpaque(false);
         profileButton.setPreferredSize(size);
 
         profileButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent ae) {
                 calendar.setVisible(false);
+                settingsPanel.setVisible(false);
                 frame.remove(calendar);
+                frame.remove(settingsPanel);
                 frame.add(profilePanel, BorderLayout.CENTER);
                 profilePanel.setVisible(true);
             }
         });
 
         JButton logoutButton = new JButton(logoutIcon);
+        logoutButton.setOpaque(false);
         logoutButton.setPreferredSize(size);
         logoutButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent ae) {
@@ -68,9 +85,25 @@ public class Navigation extends JPanel {
             }
         });
 
+        JButton settingsButton = new JButton(settingsIcon);
+        settingsButton.setOpaque(false);
+        settingsButton.setPreferredSize(size);
+        settingsButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent ae) {
+                calendar.setVisible(false);
+                profilePanel.setVisible(false);
+                frame.remove(calendar);
+                frame.remove(profilePanel);
+                frame.add(settingsPanel, BorderLayout.CENTER);
+                settingsPanel.setVisible(true);
+            }
+        });
+
         this.add(homeButton);
         this.add(profileButton);
         this.add(logoutButton);
+        this.add(settingsButton);
+        this.setBackground(Color.PINK);
         this.setVisible(true);
     }
 }
