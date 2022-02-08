0368024d2c70faed36b3ec4a4abef53b3ae22f7c
FrostJ143 <70150272+FrostJ143@users.noreply.github.com>
2022-02-08 17:51:08 +0700

    Add: javadoc

--- TimeScheduler_v1_0/src/Event.java
+++ TimeScheduler_v1_0/src/Event.java
@@ -3,6 +3,36 @@ package src;
 import java.util.ArrayList;
 import java.util.Date;
 
+/**
+ * 
+ * This class is used to create a {@link Event} object.
+ * <p>
+ * <p>
+ * The {@code Event} object contains the following:
+ * <p>
+ * - A unique ID
+ * <p>
+ * - A user ID of the user creates object
+ * <p>
+ * - A title
+ * <p>
+ * - A description
+ * <p>
+ * - A date
+ * <p>
+ * - A remind
+ * <p>
+ * - A location
+ * <p>
+ * - A duration
+ * <p>
+ * - A priority
+ * <p>
+ * - A list of participants
+ * 
+ * <p>
+ * @author Sang Doan Tan 1370137
+ */
 public class Event {
     private String ID;
     private String userID;
@@ -15,6 +45,14 @@ public class Event {
     private int priority;
     private ArrayList<String> participants;
 
+    /**
+    * Constructor for the {@link Event} with 4 attributes
+    * 
+    * @param userID    The ID of the user created event
+    * @param title     The title of the event
+    * @param date      The date of the event
+    * @param duration  The duration of the event
+    */
     public Event(String userID, String title, Date date, int duration) {
         this.userID = userID;
         this.title = title;
@@ -22,6 +60,19 @@ public class Event {
         this.duration = duration;
     }
 
+    /**
+    * Constructor for the {@link Event} with participants
+    * 
+    * @param userID        The ID of the user created event
+    * @param title         The title of the event
+    * @param description   The description of the event    
+    * @param participants  The list of participant of the event
+    * @param date          The date of the event
+    * @param remind        The remind of the event
+    * @param location      The location of the event
+    * @param duration      The duration of the event   
+    * @param priority      The priority of the event
+    */
     public Event(String userID, String title, String description, ArrayList<String> participants, Date date,
             Date remind, String location, int duration,
             int priority) {
@@ -36,6 +87,17 @@ public class Event {
         this.priority = priority;
     }
 
+    /** 
+    * Constructor for the {@link Event} without participants
+    * @param userID        The ID of the user created event
+    * @param title         The title of the event
+    * @param description   The description of the event 
+    * @param date          The date of the event
+    * @param remind        The remind of the event
+    * @param location      The location of the event
+    * @param duration      The duration of the event   
+    * @param priority      The priority of the event
+    */
     public Event(String userID, String title, String description, Date date, Date remind, String location, int duration,
             int priority) {
         this.userID = userID;
@@ -48,6 +110,19 @@ public class Event {
         this.priority = priority;
     }
 
+    /** 
+    * Constructor for the {@link Event} to edit {@code Event} and without participants
+    * 
+    * @param id            The ID of the event need to be edit
+    * @param userID        The ID of the user created event
+    * @param title         The title of the event
+    * @param description   The description of the event 
+    * @param date          The date of the event
+    * @param remind        The remind of the event
+    * @param location      The location of the event
+    * @param duration      The duration of the event   
+    * @param priority      The priority of the event
+    */
     public Event(String id, String userID, String title, String description, Date date, Date remind, String location,
             int duration, int priority) {
         this.ID = id;
