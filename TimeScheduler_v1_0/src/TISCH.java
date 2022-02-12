8c3d18c5808fe8bd7b0284989db09289797b8aac
Thái Hoàng Tâm <75922889+RoyalHeart@users.noreply.github.com>
2022-02-12 21:59:22 +0100

    Add: javadoc

--- TimeScheduler_v1_0/src/TISCH.java
+++ TimeScheduler_v1_0/src/TISCH.java
@@ -1,7 +1,15 @@
 package src;
 
+import javax.swing.UIManager;
+
+/**
+ * Main class for the Time Scheduler.
+ *
+ * @author Tam Thai Hoang
+ */
 public class TISCH {
     public static void main(String[] args) throws Exception {
+        UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
             public void run() {
                 // create and show the GUI
