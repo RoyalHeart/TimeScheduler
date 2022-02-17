c4d415a25cf811ce76951d0e63de372996c21aa8
Thái Hoàng Tâm <75922889+RoyalHeart@users.noreply.github.com>
2022-02-17 09:31:53 +0100

    Add: refresh database connection

--- TimeScheduler_v1_0/src/MainFrame.java
+++ TimeScheduler_v1_0/src/MainFrame.java
@@ -44,6 +44,7 @@ public class MainFrame extends JFrame {
         AddEventDialog addEventDialog = new AddEventDialog(user, calendar);
         SchedulerJava.createScheduler();
         SchedulerJava.setFutureRemind(user);
+        SchedulerJava.refreshConnection();
         this.setIconImage(icon.getImage());
         this.setSize(700, 700);
         this.setPreferredSize(new Dimension(700, 700));
